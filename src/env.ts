52e9c92b99391a9992bd6fe01efc5be6e1c1a510
bobosneefdev <bobosneefbusiness@gmail.com>
2025-05-23 06:48:35 -0700

    update dependencies

--- src/env.ts
+++ src/env.ts
@@ -1,13 +1,21 @@
 import { ZodEnv } from "@bobosneefdev/zodutil";
-import fs from "fs";
 import { z } from "zod";
 import { ZongoLogLevel } from "./types";
 
 export const kZongoEnv = new ZodEnv({
-    ZONGO_MONGO_URI: z.string()
-        .default("mongodb://localhost:27017"),
-    ZONGO_BACKUP_DIR: z.string()
-        .default("./ZongoDB/backups"),
-    ZONGO_LOG_LEVEL: z.nativeEnum(ZongoLogLevel)
-        .default(ZongoLogLevel.INFO),
+    ZONGO_MONGO_URI: {
+        schema: z.string()
+            .default("mongodb://localhost:27017"),
+        type: "throwOnStartup",
+    },
+    ZONGO_BACKUP_DIR: {
+        schema: z.string()
+            .default("./ZongoDB/backups"),
+        type: "throwOnStartup",
+    },
+    ZONGO_LOG_LEVEL: {
+        schema: z.nativeEnum(ZongoLogLevel)
+            .default(ZongoLogLevel.INFO),
+        type: "throwOnStartup",
+    },
 });
